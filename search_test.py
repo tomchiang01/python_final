9c77e803162162270e4cc757c45c48f773336e9d
tomchiang01 <55355909+tomchiang01@users.noreply.github.com>
2022-11-28 21:17:04 +0800

    Add files via upload

--- search_test.py
+++ search_test.py
@@ -1,4 +1,4 @@
 from search import search
 
 
-search.search("cat", limit=10,  output_dir="temp", adult_filter_off=False, force_replace=False, timeout=5, verbose=True)
+search.search("cat", limit=10,  output_dir="temp", verbose=True)

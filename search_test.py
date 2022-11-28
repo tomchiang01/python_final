d67c7f97769200ce0f6323f959f8f8b16e3165fb
tomchiang01 <55355909+tomchiang01@users.noreply.github.com>
2022-11-29 01:01:59 +0800

    Add files via upload

--- search_test.py
+++ search_test.py
@@ -1,4 +1,9 @@
 from search import search
+import time
 
 
-search.search("cat", limit=10,  output_dir="temp", verbose=True)
+
+if __name__ == '__main__':
+    S = search.Search("cat", limit=20,  output_dir="temp", verbose=True)
+    time.sleep(10)
+    S.kill()

ae6e7075458b7c5bb685683db18add9ba8921fae
tomchiang01 <55355909+tomchiang01@users.noreply.github.com>
2022-11-28 18:18:00 +0800

    Delete search.py

--- search.py
+++
@@ -1,8 +0,0 @@
-from bing_image_downloader.downloader import download
-
-
-def search(key_word,dir_name,size):
-    download(key_word, limit=size,  output_dir=dir_name, adult_filter_off=False, force_replace=False, timeout=60, verbose=False)
-
-if __name__ == "__main__":
-    search("cat","temp",10)

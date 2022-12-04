fcc6552b6cc3def0e72ca0f8cea3b88ddaee8849
jimmyb08901017 <56395377+jimmyb08901017@users.noreply.github.com>
2022-12-05 01:15:45 +0800

    :art: Move setting Button

--- v4/Setting_Window.py
+++ v4/Setting_Window.py
@@ -20,8 +20,8 @@ class Setting_Window:
             self.e_key_word_text.set(self.widget.key_word)
         self.e_key_word.place(x = 100, y = 10)
         
-        self.b_set_key = tk.Button(self.master, text ="確定", command = self.set_key_word)
-        self.b_set_key.place(x = 250, y = 8)
+        #self.b_set_key = tk.Button(self.master, text ="確定", command = self.set_key_word)
+        #self.b_set_key.place(x = 250, y = 8)
         
         self.l_img_refresh_time = tk.Label(self.master, text = "圖片刷新時間: ")
         self.l_img_refresh_time.place(x = 10, y = 40)
@@ -35,8 +35,8 @@ class Setting_Window:
         self.l_img_refresh_time2 = tk.Label(self.master, text = "分鐘")
         self.l_img_refresh_time2.place(x = 210, y = 40)
         
-        self.b_set_time = tk.Button(self.master, text ="確定", command = self.set_time)
-        self.b_set_time.place(x = 250, y = 38)
+        #self.b_set_time = tk.Button(self.master, text ="確定", command = self.set_time)
+        #self.b_set_time.place(x = 250, y = 38)
 
 
         self.l_temp_size = tk.Label(self.master, text = "圖庫大小: ")
@@ -46,8 +46,20 @@ class Setting_Window:
         self.e_temp_size_text.set(str(self.widget.temp_size))
         self.e_temp_size.place(x = 100, y = 70)
 
-        self.b_set_temp_size = tk.Button(self.master, text ="確定", command = self.set_temp_size)
-        self.b_set_temp_size.place(x = 250, y = 68)
+        #self.b_set_temp_size = tk.Button(self.master, text ="確定", command = self.set_temp_size)
+        #self.b_set_temp_size.place(x = 250, y = 68)
+        
+        self.b_set_all = tk.Button(self.master, text ="OK", command = self.set_all)
+        self.b_set_all.place(x = 200, y = 250)
+        
+        self.b_cancel = tk.Button(self.master, text ="cancel", command = self.master.destroy)
+        self.b_cancel.place(x = 235, y = 250)
+        
+    def set_all(self):
+        self.set_key_word()
+        self.set_time()
+        self.set_temp_size()
+        
 
     def set_key_word(self):
         path=os.path.abspath(".\\temp")
